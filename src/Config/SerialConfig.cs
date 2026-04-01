ad1b82f57983990a3dda797754ea00e02b263b1e
Oliver Raider <raider.o@gmx.de>
2026-04-01 10:54:43 +0200

    refactor(config): remove required from SerialConfig.Port/BaudRate, add defaults

--- src/Config/SerialConfig.cs
+++ src/Config/SerialConfig.cs
@@ -5,8 +5,8 @@ namespace SerialSavant.Config;
 
 public sealed record SerialConfig
 {
-    public required string Port { get; init; }
-    public required int BaudRate { get; init; }
+    public string Port { get; init; } = "/dev/ttyUSB0";
+    public int BaudRate { get; init; } = 115200;
     public int ReconnectBaseDelayMs { get; init; } = 500;
     public int ReconnectMaxDelayMs { get; init; } = 30_000;
     public int ReconnectMaxAttempts { get; init; } = 10;
